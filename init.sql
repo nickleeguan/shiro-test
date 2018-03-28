58f9db51c19b7977c4d94641eb46208c470b8c3b
nickleeguan <469522336@qq.com>
2018-03-28 17:53:31 +0800

    shiro权限配置

---
+++ init.sql
@@ -0,0 +1,49 @@
+# --权限表
+CREATE TABLE permission(
+  pid INT(11) not NULL AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL DEFAULT '',
+  url VARCHAR(255) DEFAULT '',
+  PRIMARY KEY (pid)
+)ENGINE = InnoDB DEFAULT CHARSET = utf8;
+INSERT INTO permission VALUES ('1','add','');
+INSERT INTO permission VALUES ('2','delete','');
+INSERT INTO permission VALUES ('3','edit','');
+INSERT INTO permission VALUES ('4','query','');
+
+
+# 角色表
+CREATE TABLE role(
+  rid INT(11) NOT NULL AUTO_INCREMENT,
+  rname VARCHAR(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (rid)
+)ENGINE = InnoDB DEFAULT CHARSET = utf8;
+INSERT INTO role VALUES ('1','admin');
+INSERT INTO role VALUES ('2', 'customer');
+
+# 用户表
+CREATE TABLE user(
+  uid INT(11) NOT NULL AUTO_INCREMENT,
+  username VARCHAR(255) NOT NULL DEFAULT '',
+  password VARCHAR(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (uid)
+)ENGINE = InnoDB DEFAULT CHARSET = utf8;
+INSERT INTO user VALUES ('1','admin','123');
+INSERT INTO user VALUES ('2','demo','123');
+
+# 权限角色关系表
+CREATE TABLE permission_role(
+  rid INT(11) NOT NULL,
+  pid INT(11) NOT NULL,
+  KEY idx_rid(rid),
+  KEY idx_pid(pid)
+)ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+
+# 角色用户关系表
+CREATE TABLE user_role(
+  uid INT(11) NOT NULL,
+  rid INT(11) NOT NULL,
+  KEY idx_uid(uid),
+  KEY idx_rid(rid)
+)ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
